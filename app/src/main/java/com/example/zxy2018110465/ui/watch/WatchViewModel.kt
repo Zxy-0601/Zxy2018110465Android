7734ccdbc606c3865a91f444fe39c1e541f12082
Zxy-0601 <55048376+Zxy-0601@users.noreply.github.com>
2020-12-25 14:30:19 +0800

    watchVersion界面2.2

--- app/src/main/java/com/example/zxy2018110465/ui/watch/WatchViewModel.kt
+++ app/src/main/java/com/example/zxy2018110465/ui/watch/WatchViewModel.kt
@@ -1,7 +1,43 @@
 package com.example.zxy2018110465.ui.watch
 
+import android.os.Handler
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class WatchViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class WatchViewModel :ViewModel(){
+    private var _seconds:MutableLiveData<Int> = MutableLiveData()
+    private var running=false
+    val seconds:LiveData<Int> = _seconds
+    init {
+        runTimer()
+    }
+
+    fun start(){
+        running = true
+    }
+
+    fun stop(){
+        running = false
+    }
+
+    fun restart(){
+        running = true
+        _seconds.value = 0
+    }
+
+    fun runTimer(){
+        val handler  =  Handler()
+        val runnable=object :Runnable{
+            override fun run() {
+                if(running){
+                    val sec = _seconds.value ?:0
+                    _seconds.value = sec + 1
+                }
+                handler.postDelayed(this,1000)
+            }
+        }
+        handler.post (runnable)
+    }
+
 }
