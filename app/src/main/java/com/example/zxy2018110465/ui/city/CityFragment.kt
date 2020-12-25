c7bb48ed5eeb7979929449f52f20b6718a2a1439
Zxy-0601 <55048376+Zxy-0601@users.noreply.github.com>
2020-12-25 22:35:26 +0800

    weatherVersion3.4

--- app/src/main/java/com/example/zxy2018110465/ui/city/CityFragment.kt
+++ app/src/main/java/com/example/zxy2018110465/ui/city/CityFragment.kt
@@ -1,30 +1,42 @@
 package com.example.zxy2018110465.ui.city
 
+import android.app.Application
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.zxy2018110465.R
 import kotlinx.android.synthetic.main.fragment_city.*
 
-class CityFragment : AppCompatActivity() {
+class CityFragment : Fragment() {
+    companion object {
+        fun newInstance() = CityFragment()
+    }
 
-    lateinit var viewModel:CityViewModel
+    private lateinit var viewModel: CityViewModel
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_city, container, false)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_city)
-        viewModel = ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory(application)).get(CityViewModel::class.java)
+        viewModel = ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory(Application())).get(CityViewModel::class.java)
         viewModel.cities.observe(this, Observer {
             val cities = it
-            val adapter = ArrayAdapter<CityItem>(this, android.R.layout.simple_list_item_1, cities)//准备适配器
+            val adapter = ArrayAdapter<CityItem>(this.requireContext(), android.R.layout.simple_list_item_1, cities)//准备适配器
             listView.adapter = adapter
             listView.setOnItemClickListener { _, _, position, _ ->
                 val CityCode = cities[position].city_code
                 //构建意图
-                val intent = Intent(this, Main2Activity::class.java)
+                val intent = Intent(this.context, Main2Activity::class.java)
                 intent.putExtra("city_code", CityCode)
                 startActivity(intent)
             }
