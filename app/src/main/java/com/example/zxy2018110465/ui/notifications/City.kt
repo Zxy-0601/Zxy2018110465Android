86ef405f81639b46eb7e74939aa4f56baf4ee5bd
Zxy-0601 <55048376+Zxy-0601@users.noreply.github.com>
2020-12-25 17:44:36 +0800

    weatherVersion3.2

--- app/src/main/java/com/example/zxy2018110465/ui/notifications/City.kt
+++ app/src/main/java/com/example/zxy2018110465/ui/notifications/City.kt
@@ -1,5 +1,3 @@
 package com.example.zxy2018110465.ui.notifications
 
-import com.example.zxy2018110465.ui.notifications.CityItem
-
 class City : ArrayList<CityItem>()
--- app/src/main/java/com/example/zxy2018110465/ui/notifications/CityFragment.kt
+++ app/src/main/java/com/example/zxy2018110465/ui/notifications/CityFragment.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.zxy2018110465.R
