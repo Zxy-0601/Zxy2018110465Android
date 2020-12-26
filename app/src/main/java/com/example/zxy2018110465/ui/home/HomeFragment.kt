fcda4e014240483835d25bd67bd5276f41116db8
Zxy-0601 <55048376+Zxy-0601@users.noreply.github.com>
2020-12-26 22:17:48 +0800

    homeVersion5.0

--- app/src/main/java/com/example/zxy2018110465/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/zxy2018110465/ui/home/HomeFragment.kt
@@ -1,27 +1,37 @@
 package com.example.zxy2018110465.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.zxy2018110465.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
-
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
+            inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        return root
+        Log.d("BlankFragment1","onCreateView")
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button = view.findViewById<Button>(R.id.button)
+        val imageView = view.findViewById<ImageView>(R.id.imageView)
+        button.setOnClickListener {
+            textView.text = getString(R.string.change)
+        }
     }
 }
