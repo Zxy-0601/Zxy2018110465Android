a2dcd0d1afcb73f2043583a26d1823d945c4baaf
Zxy-0601 <55048376+Zxy-0601@users.noreply.github.com>
2020-12-26 17:20:36 +0800

    cardVersion4.0

--- app/src/main/java/com/example/zxy2018110465/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/zxy2018110465/ui/dashboard/DashboardFragment.kt
@@ -1,31 +1,62 @@
 package com.example.zxy2018110465.ui.dashboard
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.fourth.model.CardMatchingGame
 import com.example.zxy2018110465.R
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
-class DashboardFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+
+class DashboardFragment : Fragment(){
+    companion object {
+        var game: CardMatchingGame = CardMatchingGame(24)
+    }
+
+
+    lateinit var adapter: DashboardViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
+            inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        adapter = DashboardViewModel(game)
+        recyclerView.adapter = adapter
+
+        val configure = resources.configuration
+        if (configure.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            recyclerView.layoutManager = GridLayoutManager(this.context, 4)
+        } else {
+            recyclerView.layoutManager = GridLayoutManager(this.context, 6)
+        }
+
+        adapter.setOnClickListener {
+            game.chooseCardAtIndex(it)
+            updateUI()
+        }
+
+        updateUI()
+
+        reset.setOnClickListener {
+            game.reset()
+            updateUI()
+        }
+    }
+
+    fun updateUI() {
+        adapter.notifyDataSetChanged()
+        score.text = String.format("%s%d", "Score:", game.score)
+        score.text = "Score:" + game.score
+    }
+
 }
--- app/src/main/java/com/example/zxy2018110465/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/zxy2018110465/ui/dashboard/DashboardViewModel.kt
@@ -1,13 +1,48 @@
 package com.example.zxy2018110465.ui.dashboard
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fourth.model.CardMatchingGame
+import com.example.zxy2018110465.R
 
-class DashboardViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+class DashboardViewModel(val game: CardMatchingGame): RecyclerView.Adapter<DashboardViewModel.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.item_layout,parent,false)
+        return ViewHolder(view)
     }
-    val text: LiveData<String> = _text
+
+    override fun getItemCount(): Int {
+        return game.cards.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val card=game.cardAtIndex(position)
+        holder.cardButton.isEnabled=!card.isMatched
+        if (card.isChosen){
+            holder.cardButton.text=card.toString()
+            holder.cardButton.setBackgroundColor(Color.WHITE)
+        }else{
+            holder.cardButton.text=""
+            holder.cardButton.setBackgroundColor(R.drawable.back)
+        }
+        holder.cardButton.setOnClickListener{
+            mListener?.invoke(position)
+        }
+    }
+
+    class ViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){
+        val cardButton:Button
+        init {
+            cardButton=itemView.findViewById(R.id.button)
+        }
+    }
+
+    var mListener:((Int)->Unit)?=null
+    fun setOnClickListener(l:(Int)->Unit){
+        mListener=l
+    }
 }
